27e787b98c351a16b1f998c2a5f994682282516a
mahabur  rahman <annur4395@gmail.com>
2022-08-16 21:36:18 +0600

    user register func async/await

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,43 +1,79 @@
 const UserModel = require("../models/userSchema");
 
-// POST | REGISTER USER
-const userRegister = (req, res) => {
+// ############################## POST | REGISTER USER
+
+// const userRegister = (req, res) => {
+//   const { name, email, phone, work, password, cpassword } = req.body;
+
+//   if (!name || !email || !phone || !work || !password || !cpassword) {
+//     return res.status(422).json({ error: "plz fill the all field!" });
+//   }
+
+//   UserModel.findOne({ email: email })
+//     .then((userExist) => {
+//       if (userExist) {
+//         return res.status(422).json({ error: "Email already exist!" });
+//       }
+
+//       const user = new UserModel({
+//         name,
+//         email,
+//         phone,
+//         work,
+//         password,
+//         cpassword,
+//       });
+
+//       user
+//         .save()
+//         .then(() => {
+//           return res
+//             .status(201)
+//             .json({ message: "user registered successful!" });
+//         })
+//         .catch((err) => {
+//           return res.status(500).json({ error: err });
+//         });
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//     });
+// };
+
+// ----------------- async a await
+const userRegister = async (req, res) => {
   const { name, email, phone, work, password, cpassword } = req.body;
 
   if (!name || !email || !phone || !work || !password || !cpassword) {
     return res.status(422).json({ error: "plz fill the all field!" });
   }
 
-  UserModel.findOne({ email: email })
-    .then((userExist) => {
-      if (userExist) {
-        return res.status(422).json({ error: "Email already exist!" });
-      }
+  const preUser = await UserModel.findOne({ email: email });
 
-      const user = new UserModel({
-        name,
-        email,
-        phone,
-        work,
-        password,
-        cpassword,
-      });
+  if (preUser) {
+    return res.status(422).json({ error: "Email already exist!" });
+  }
 
-      user
-        .save()
-        .then(() => {
-          return res
-            .status(201)
-            .json({ message: "user registered successful!" });
-        })
-        .catch((err) => {
-          return res.status(500).json({ error: err });
-        });
-    })
-    .catch((err) => {
-      console.log(err);
+  try {
+    const user = await UserModel({
+      name,
+      email,
+      phone,
+      work,
+      password,
+      cpassword,
     });
+
+    // save on db
+    const userData = await user.save();
+
+    return res.status(201).json({ user: userData });
+  } catch (err) {
+    return res.status(500).json({ error: err });
+  }
 };
+
+// ##############################
 
 // export
 module.exports = { userRegister };
