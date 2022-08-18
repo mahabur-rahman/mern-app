a5e5b874ad0146c4416591e2161ffb266068f2f7
mahabur  rahman <annur4395@gmail.com>
2022-08-18 08:22:22 +0600

    store data in register

--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -1,8 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import { Container, Row, Col, Form } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 const Register = () => {
+  const [info, setInfo] = useState({
+    name: "",
+    email: "",
+    phone: "",
+    work: "",
+    password: "",
+    cpassword: "",
+  });
+
+  // handleChange
+  const handleChange = (e) => {
+    let name, value;
+    name = e.target.name;
+    value = e.target.value;
+
+    setInfo({ ...info, [name]: value });
+  };
+
   return (
     <div>
       <section id="register" className="py-5">
@@ -16,29 +34,62 @@ const Register = () => {
                 <Col xl={6} className="mx-auto">
                   <Form method="POST">
                     <Form.Group className="mb-3" controlId="name">
-                      <Form.Control type="text" placeholder="name" />
+                      <Form.Control
+                        type="text"
+                        placeholder="name"
+                        name="name"
+                        value={info.name}
+                        onChange={handleChange}
+                      />
                     </Form.Group>
 
                     <Form.Group className="mb-3" controlId="email">
-                      <Form.Control type="email" placeholder="email" />
+                      <Form.Control
+                        type="email"
+                        placeholder="email"
+                        name="email"
+                        value={info.email}
+                        onChange={handleChange}
+                      />
                     </Form.Group>
 
                     <Form.Group className="mb-3" controlId="phone">
-                      <Form.Control type="number" placeholder="phone" />
+                      <Form.Control
+                        type="number"
+                        placeholder="phone"
+                        name="phone"
+                        value={info.phone}
+                        onChange={handleChange}
+                      />
                     </Form.Group>
 
                     <Form.Group className="mb-3" controlId="work">
-                      <Form.Control type="text" placeholder="work" />
+                      <Form.Control
+                        type="text"
+                        placeholder="work"
+                        name="work"
+                        value={info.work}
+                        onChange={handleChange}
+                      />
                     </Form.Group>
 
                     <Form.Group className="mb-3" controlId="password">
-                      <Form.Control type="password" placeholder="password" />
+                      <Form.Control
+                        type="password"
+                        placeholder="password"
+                        name="password"
+                        value={info.password}
+                        onChange={handleChange}
+                      />
                     </Form.Group>
 
                     <Form.Group className="mb-3" controlId="cpassword">
                       <Form.Control
                         type="password"
                         placeholder="confirm password"
+                        name="cpassword"
+                        value={info.cpassword}
+                        onChange={handleChange}
                       />
                     </Form.Group>
                     <div className="text-end">
