3f6278b84b7fb6d966fee0de95e32cf2d9aabcc1
mahabur  rahman <annur4395@gmail.com>
2022-08-20 20:03:11 +0600

    fetch data on home page

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,13 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const Home = () => {
+  const [userName, setUserName] = useState("");
+  const [show, setShow] = useState(false);
+
+  const getData = async () => {
+    try {
+      const res = await axios.get("/getData");
+      setUserName(res.data.name);
+      setShow(true);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <>
       <div className="my-5 py-5 text-center">
         <h1 className="text-uppercase mt-5 pt-5">Welcome to home page</h1>
-        <h1>Mahabur Rahman</h1>
+        <h1>{userName}</h1>
         <h4 className="w-50 mx-auto">
-          Happy to see you back, We are the mern developer
+          {show ? "Happy to see you back" : "We are the mern developer"}
         </h4>
       </div>
     </>
